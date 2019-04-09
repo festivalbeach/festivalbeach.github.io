0076d8023424a835f51650ae5226eab52836d962
eweiss97 <eweiss.ism@gmail.com>
2019-04-08 23:52:36 -0500

    Added code to fetch plantinfo from google sheets and store it in a dictionary of plant name to its information.

--- food-forest/src/App.js
+++ food-forest/src/App.js
@@ -1,27 +1,59 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Tabletop from 'tabletop';
 
 class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      plantInfo: {}
+    }
+  }
+
+  // Loads plant info on initialization into a map of name to object
+  componentDidMount() {
+    Tabletop.init({
+      key: '1ecF8O2AxaI3DDI8A-o3B7zvzAeYYNU9HDvsshXAAmwc',
+      callback: data => {
+        let info = {};
+        data.forEach(dat => {
+          info[dat['Label']] = dat;
+        });
+        this.setState({plantInfo: info});
+      },
+      simpleSheet: true
+    })
+  }
+
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    if (Object.keys(this.state.plantInfo).length === 0) {
+      return (<p>Loading...</p>);
+    }
+    else
+      console.log(this.state.plantInfo['Peach']);
+      return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+
+            {Object.keys(this.state.plantInfo).map((key, value) => (
+              <p>{key} {this.state.plantInfo[key]['Family']}</p>
+            ))}
+            {this.state.plantInfo['Peach']['Label']}
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+      );
   }
 }
 
