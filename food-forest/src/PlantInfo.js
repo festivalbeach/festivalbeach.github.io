a4b0f6fe6a8035e7ad81e327f9eda7c6b4268acd
wip34 <wip0304@gmail.com>
2019-04-28 16:55:04 -0500

    plant modal changes, changes color based on bloom color, might be too tacky. Can change to a static color of food festival colors

--- food-forest/src/PlantInfo.js
+++ food-forest/src/PlantInfo.js
@@ -17,6 +17,16 @@ class PlantInfo extends React.Component {
     }));
   }
 
+  displayTF(column) {
+    if(column == "TRUE"){
+      return "Yes";
+    }
+    else if(column == "FALSE"){
+      return "No";
+    }
+    return "";
+  }
+
    render() {
     return (
       <div>
@@ -25,25 +35,28 @@ class PlantInfo extends React.Component {
           <div style={{color: 'white', fontSize: '.8em', position: 'relative', right: '2px'}}>{this.props.plant}</div>
         </icon>
         <Modal scrollable isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
-          <ModalHeader toggle={this.toggle}>{this.props.plant}</ModalHeader>
+          <ModalHeader style={
+            {backgroundColor: ((this.props.plantInfoProp['Bloom Color'] == "" || this.props.plantInfoProp['Bloom Color'] == "White") ? "lightblue" : this.props.plantInfoProp['Bloom Color'])}} 
+            toggle={this.toggle}>{this.props.plant}
+          </ModalHeader>
           <ModalBody>
               Name: {this.props.plantInfoProp['Label']} <br />
-              Genus: {this.props.plantInfoProp['Genus']} <br />
-              Family: {this.props.plantInfoProp['Family']} <br />
-              Cousin: {this.props.plantInfoProp['Cousin']} <br />
-              Species: {this.props.plantInfoProp['Species']} <br />
-              Strata: {this.props.plantInfoProp['Species']} <br />
-              Fruit Type: {this.props.plantInfoProp['Fruit Type']} <br />
-              Fruit Edibility: {this.props.plantInfoProp['Edible (fruit) Y/N']} <br />
+              Taxonomy: {this.props.plantInfoProp['Family']} {this.props.plantInfoProp['Genus']} {this.props.plantInfoProp['Species']}<br />
               Bloom Color: {this.props.plantInfoProp['Bloom Color']} <br />
-              Flower Edibility: {this.props.plantInfoProp['Edible Flower  Y/N']} <br />
-              Leaf Edibility: {this.props.plantInfoProp['Edible Leaf']} <br />
-              Bark Edibility: {this.props.plantInfoProp['Edible Bark Y/N']} <br />
-              Seed Edibility: {this.props.plantInfoProp['Edible Seed']} <br />
+              <hr color = "black"></hr>
+              Harvest Start: {this.props.plantInfoProp['Harvest Start']} <br />
+              Harvest End: {this.props.plantInfoProp['Harvest End']} <br />
+              <hr color = "black" width = "90%"></hr>
+              Fruit Edibility: {this.displayTF(this.props.plantInfoProp['Edible (fruit) Y/N'])} <br />
+              Flower Edibility: {this.displayTF(this.props.plantInfoProp['Edible Flower  Y/N'])} <br />
+              Leaf Edibility: {this.displayTF(this.props.plantInfoProp['Edible Leaf'])} <br />
+              Bark Edibility: {this.displayTF(this.props.plantInfoProp['Edible Bark Y/N'])} <br />
+              Seed Edibility: {this.displayTF(this.props.plantInfoProp['Edible Seed'])} <br />
               Overall Toxicity Rating (1-4): {this.props.plantInfoProp['Toxicity (Rating: 1-4)']} <br />
+              Fruit Type: {this.props.plantInfoProp['Fruit Type']} <br />
               Water Use: {this.props.plantInfoProp['Water Use']} <br />
           </ModalBody>
-          <ModalFooter>
+          <ModalFooter style={{backgroundColor: ((this.props.plantInfoProp['Bloom Color'] == "" || this.props.plantInfoProp['Bloom Color'] == "White") ? "lightblue" : this.props.plantInfoProp['Bloom Color'])}}>
             <Button color="secondary" onClick={this.toggle}>Cancel</Button>
           </ModalFooter>
         </Modal>
